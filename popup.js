2cb486111d335b350a7f0a1a13397119bae4d192
RonBoy <123363908+aaronDev28@users.noreply.github.com>
2023-04-07 02:32:24 +0530

    test commit

--- popup.js
+++ popup.js
@@ -1,4 +1,39 @@
-var { sjcl } = require('./sjcl.min.js')
+"use strict";
+var sjcl = {
+  cipher: {},
+  hash: {},
+  keyexchange: {},
+  mode: {},
+  misc: {},
+  codec: {},
+  exception: {
+    corrupt: function (a) {
+      this.toString = function () {
+        return "CORRUPT: " + this.message;
+      };
+      this.message = a;
+    },
+    invalid: function (a) {
+      this.toString = function () {
+        return "INVALID: " + this.message;
+      };
+      this.message = a;
+    },
+    bug: function (a) {
+      this.toString = function () {
+        return "BUG: " + this.message;
+      };
+      this.message = a;
+    },
+    notReady: function (a) {
+      this.toString = function () {
+        return "NOT READY: " + this.message;
+      };
+      this.message = a;
+    },
+  },
+};
+if (typeof module != "undefined" && module.exports) module.exports = sjcl;
 
 //encode the image and download it
 var enc = function() {
